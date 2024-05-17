36cf258abd79a03806c30295bc008a21b2405d9b
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2024-05-17 03:03:07 -0700

    completed the form

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mwequdymbuaffcfuejya.supabase.co";
+export const supabaseUrl = "https://mwequdymbuaffcfuejya.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13ZXF1ZHltYnVhZmZjZnVlanlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MDQxMjgsImV4cCI6MjAzMTE4MDEyOH0.BNvuQGfCV5jAlaPSZIqXEmec029cr6vvcpd9DtknriM";
 const supabase = createClient(supabaseUrl, supabaseKey);
